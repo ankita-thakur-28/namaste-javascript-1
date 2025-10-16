9ca96d8a383a3acfab5d366df5a366ead6ae2a42
ankita-thakur-28 <ankitathakurmfp@gmail.com>
2025-10-16 12:58:18 +0900

    notes-11

---
+++ episode-11/script.js
@@ -0,0 +1,20 @@
+function x(){
+    var i = 500;
+    setTimeout(function() {
+        console.log(i);
+}, 5000 );
+   console.log("Namste Javascript")
+}
+x();
+
+
+//
+function x() {
+for (var i = 1; i <= 5; i++) {
+setTimeout (function () {
+console. log(i);
+}, i * 1000);
+}
+console.log ("Namaste JavaScript");
+}
+x();
